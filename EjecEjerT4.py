f4df91c620b45f5ee03f859dcb0c3ab514664845
ntorrecillas10 <nalascobic7@gmail.com>
2024-11-07 16:31:50 +0100

    ej8not

--- EjecEjerT4.py
+++ EjecEjerT4.py
@@ -2,3 +2,9 @@ from EjerciciosT4 import *
 
 ej1(10,15)
 ej2()
+ej3()
+ej4([1,2,3,4],[2,3,4,5])
+ej5()
+ej6()
+ej7()
+ej8()
--- EjerciciosT4.py
+++ EjerciciosT4.py
@@ -20,9 +20,89 @@ def ej2():
     milista = []
 
     for palabra in lista:
-        i = 0
-        if len(lista[i].split(None)) > 6:
+        if len(palabra).__int__() > 6:
             milista.append(palabra)
-            i *= 1
 
     print(milista)
+
+
+def ej3():
+    lista = [3,4,5,6,7]
+    lista_cuadrados = []
+
+    for num in lista:
+        cuadrado = num*num
+        lista_cuadrados.append(cuadrado)
+
+    print(lista_cuadrados)
+
+def ej4(l1 : list, l2 : list):
+    lista_final = l1
+
+    for elem in l2:
+        if elem not in l1:
+            lista_final.append(elem)
+
+    print(lista_final)
+
+
+def ej5():
+    lista = ["pesimo", "ultimo", "mariquita", "sobador", "tornado","pesimo", "ultimo", "mariquita", "sobador", "tornado"]
+    palabra = "ultimo"
+
+    for elem in lista:
+        if palabra == elem:
+            lista.remove(elem)
+
+    print(lista)
+
+
+def ej6():
+    lista1 = [1, 2, 3, 4, 5]
+    lista2 = [0, 1]
+    lista_nueva = []
+
+    for i, elem in enumerate(lista1):
+        if i not in lista2:
+            lista_nueva.append(elem)
+
+    print(lista_nueva)
+
+
+def ej7():
+    lista = [1, 2, 4, 3, 2, 6, 7, 5, 3]
+    lista_devuelta = []
+    num = 2
+
+    for i, elem in enumerate(lista):
+        if elem == num:
+            lista_devuelta.append(i)
+
+    print(lista_devuelta)
+
+
+def ej8():
+    lista = ['Borja', 'borja@mail.com', 989565222, 'Marta', 'marta@mail.com', 989565333, 'Ricardo', 'raicar@mail.com', 989565444]
+    lista_datos = []
+    num = 2
+
+    for elem in lista:
+        if elem in lista:
+            lista_datos.insert(1, lista[num])
+
+    #    for dato in lista:
+    #    if dato.__pos__() == num:
+    #       lista_datos.append(dato)
+    #       num +=3
+
+    print(lista_datos)
+
+
+
+
+
+
+
+
+
+
